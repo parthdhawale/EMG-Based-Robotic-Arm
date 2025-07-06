a9185380065ef8aa57d5edc397e326eea636365e
Parth Dhawale <51280220+parthdhawale@users.noreply.github.com>
2025-07-06 19:44:30 +0530

    Update emgbased.ino

--- emgbased.ino
+++ emgbased.ino
@@ -2,24 +2,27 @@
 
 #define THRESHOLD 150 //In order to determine the state of the hand (opened/closed)
 #define EMGPIN 3 //Analog pin connected to Muscle Sensor V3 Board
-#define PINKYPIN  3 //Digital pin used by Little servo
-#define RINGPIN    5 //Digital pin used by Ring servo
-#define MIDDLEPIN  6 //Digital pin used by Middle servo
-#define INDEXPIN   9 //Digital pin used by Index servo
-#define THUMBPIN  10 //Digital pin used by Thumb servo
+#define FINGER1PIN 3 //Digital pin used by Little servo
+#define FINGER2PIN 5 //Digital pin used by FINGER2 servo
+#define FINGER3PIN 6 //Digital pin used by FINGER3 servo
+#define FINGER4PIN 9 //Digital pin used by FINGER4 servo
+#define FINGER5PIN 10 //Digital pin used by FINGER5 servo
 
 //Constants used to open and close the fingers
-#define PINKY 1 
-#define RING   2
-#define MIDDLE 3
-#define INDEX  4
-#define THUMB  5
+//FINGER1 Corresponds to pinky finger and FINGER5 Corresponds to FINGER5 finger
+#define FINGER1 1 //Based on EMG sensor placement on the left hand
+#define FINGER2 2
+#define FINGER3 3
+#define FINGER4 4
+#define FINGER5 5
 
-Servo pinky;         // Define servo fingers
-Servo ring;         // Define servo fingers
-Servo middle;         // Define servo fingers
-Servo index;         // Define servo fingers
-Servo thumb;         // Define servo fingers
+
+// Define servo fingers
+Servo FINGER1;        
+Servo FINGER2;         
+Servo FINGER3;         
+Servo FINGER4;         
+Servo FINGER5;         
 
 int finger;
 
@@ -27,36 +30,34 @@ int finger;
 void openhand(){for(finger = 1; finger < 6; finger++){openFinger(finger);}}
 void closehand(){for(finger = 1; finger < 6 ; finger++){closeFinger(finger);}}
 
-// You have to rewrite properly the functions to open and close the fingers 
-// according of your assembly
-// In my case, middle and index fingers are opened when servo is at 170 degrees and the others when servo is at 0 degrees
-// I have used Towardpro MG996R servos
+// You have to rewrite properly the functions to open and close the fingers according of your assembly FINGER3 and FINGER4 fingers are opened when servo is at 170 degrees and the others when servo is at 0 degrees
+// In our project MG996R servos were used
 
 void openFinger(int finger){
-  if(finger==PINKY){pinky.write(0);} // Little finger
-  else if(finger==RING){ring.write(170);}// Ring finger
-  else if(finger==MIDDLE){middle.write(170);}// Middle finger
-  else if(finger==INDEX){index.write(170);}// Index finger
-  else if(finger==THUMB){thumb.write(0);}//Thumb finger
+  if(finger==FINGER1){FINGER1.write(0);} // FINGER1 finger
+  else if(finger==FINGER2){FINGER2.write(170);}// FINGER2 finger
+  else if(finger==FINGER3){FINGER3.write(170);}// FINGER3 finger
+  else if(finger==FINGER4){FINGER4.write(170);}// FINGER4 finger
+  else if(finger==FINGER5){FINGER5.write(0);}//FINGER5 finger
 }
 
 void closeFinger(int finger){
-  if(finger==PINKY){pinky.write(170);} // Little finger 
-  else if(finger==RING){ring.write(0);}// Ring finger
-  else if(finger==MIDDLE){middle.write(0);}// Middle finger
-  else if(finger==INDEX){index.write(0);}// Index finger
-  else if(finger==THUMB){thumb.write(170);}//Thumb finger
+  if(finger==FINGER1){FINGER1.write(170);} // FINGER1 finger 
+  else if(finger==FINGER2){FINGER2.write(0);}// FINGER2 finger
+  else if(finger==FINGER3){FINGER3.write(0);}// FINGER3 finger
+  else if(finger==FINGER4){FINGER4.write(0);}// FINGER4 finger
+  else if(finger==FINGER5){FINGER5.write(170);}//FINGER5 finger
 }
 
 void setup(){
   
   Serial.begin(115200); //BAUDRATE set to 115200, remember it to set monitor serial properly (used this BaudRate and "NL&CR" option to visualize the values correctly)
 
-  pinky.attach(PINKYPIN);  // Set Little finger servo to digital pin 3
-  ring.attach(RINGPIN);  // Set Ring finger servo to digital pin 5
-  middle.attach(MIDDLEPIN);  // Set Middle finger servo to digital pin 6
-  index.attach(INDEXPIN);  // Set Index finger servo to digital pin 9
-  thumb.attach(THUMBPIN);  // Set Thumb finger servo to digital pin 10
+  FINGER1.attach(FINGER1PIN);  // Set FINGER1 finger servo to digital pin 3
+  FINGER2.attach(FINGER2PIN);  // Set FINGER2 finger servo to digital pin 5
+  FINGER3.attach(FINGER3PIN);  // Set FINGER3 finger servo to digital pin 6
+  FINGER4.attach(FINGER4PIN);  // Set FINGER4 finger servo to digital pin 9
+  FINGER5.attach(FINGER5PIN);  // Set FINGER5 finger servo to digital pin 10
 
 }//end setup
 
@@ -67,5 +68,5 @@ void loop() {// Nothing to do here, all is done in the interrupt function
       {closehand();}
   else //Otherwise the hand is open
       {openhand();}
-  Serial.println(value); //You can use serial monitor to set THRESHOLD properly, comparing the values shown when you open and close your hand
+  Serial.println(value); //You can use serial monitor to set THRESHOLD properly, compaFINGER2 the values shown when you open and close your hand
 }
